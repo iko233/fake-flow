34e8605caebd7e70546bdc076077585ae322de28
iko233 <iko@iko.ski>
2023-09-15 22:02:24 +0800

    init project

---
+++ src/main/java/ski/iko/app/fakeflow/FakeFlowApplication.java
@@ -0,0 +1,13 @@
+package ski.iko.app.fakeflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakeFlowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FakeFlowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ski/iko/app/fakeflow/FakeFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package ski.iko.app.fakeflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakeFlowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
